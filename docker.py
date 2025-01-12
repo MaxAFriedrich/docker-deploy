3c8dc3aacbf072c78cf5672ad2afc05d391d31b5
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2025-01-12 01:20:04 +0000

    finished buidling docker stuff

--- docker.py
+++ docker.py
@@ -25,27 +25,83 @@ def strip_docker_ports(docker_file: dict) -> dict:
     return docker_file
 
 
+def get_service_id(layout: list[backend_map_lib.Box],
+                   target_service: config_lib.Service, box_id: str) -> str:
+    out = ""
+    for box in layout:
+        if box.id != box_id:
+            continue
+        out = service_matches(box.services, target_service)
+
+    if out == "":
+        raise ValueError("Service not found in layout")
+
+    return out
+
+
+def service_matches(services: list[backend_map_lib.Service],
+                    target_service: config_lib.Service) -> str:
+    out = ""
+    for service in services:
+        if service.name != target_service.name:
+            continue
+        if service.proxy != target_service.protocol:
+            continue
+        if service.description != target_service.description:
+            continue
+        out = service.id
+    return out
+
+
+def get_box_id(layout: list[backend_map_lib.Box],
+               target_box: config_lib.Box) -> str:
+    out = ""
+    for box in layout:
+        if box.name != target_box.name:
+            continue
+        services_match = True
+        for service in target_box.services:
+            if service_matches(box.services, service) == "":
+                services_match = False
+                break
+        if not services_match:
+            continue
+        out = box.id
+
+    if out == "":
+        raise ValueError("Box not found in layout")
+
+    return out
+
+
 def adapt_docker_compose(
         next_port: int,
         interface: str,
-        map_boxes: list[backend_map_lib.Box],
+        backend_boxes: list[backend_map_lib.Box],
         config_boxes: list[config_lib.Box],
-        docker_file: dict
-) -> dict:
+        docker_file: dict,
+) -> (dict, list[backend_map_lib.ServiceInstance]):
     docker_file = strip_docker_ports(docker_file)
+    instance_services = []
     for config_box in config_boxes:
+        box_id = get_box_id(backend_boxes, config_box)
         for service in config_box.services:
+            service_id = get_service_id(backend_boxes, service, box_id)
             new_port = f"{interface}:{next_port}:{service.port}"
             existing_ports = docker_file["services"][config_box.name].get(
                 "ports", [])
             existing_ports.append(new_port)
             docker_file["services"][config_box.name]["ports"] = existing_ports
 
-            # TODO add to backend_map
+            instance_services.append(backend_map_lib.ServiceInstance(
+                box_id=box_id,
+                service_id=service_id,
+                host=f"{interface}:{next_port}",
+            ))
 
             next_port += 1
 
-    return docker_file
+    return docker_file, instance_services
 
 
 def create_deployment(
@@ -61,7 +117,7 @@ def create_deployment(
     docker_file = adapt_docker_compose(
         config.output.min_port,
         config.output.interface_ip,
-        backend_map.docker_boxes,
+        backend_map.layout,
         config.boxes,
         docker_file
     )
